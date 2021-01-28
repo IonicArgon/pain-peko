a209fbb1e2a73424a852985d4241d572a1e0b2f8
Marco Tan <marco.tan.200405@gmail.com>
2021-01-27 19:57:07 -0500

    auto file

---
+++ src/auto.cpp
@@ -0,0 +1,16 @@
+//* Autonomous control code
+//* Created: 1/27/2021
+//* Last modified: 1/27/2021
+
+//* Main header
+#include "main.h"
+
+//* Subsystem headers
+
+
+
+//* Autonomous callback
+void autonmous(void)
+{
+
+}
