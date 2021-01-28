df3211ce7f129e45ba90e678705039637893f1c7
Marco Tan <marco.tan.200405@gmail.com>
2021-01-27 21:40:41 -0500

    conveyor and intake code

---
+++ include/lib/hardware/succ.hpp
@@ -0,0 +1,10 @@
+//* Conveyor and intake header file
+//* Created: 1/27/2021
+//* Last modified: 1/27/2021
+
+#ifndef H_SUCC_HPP
+#define H_SUCC_HPP
+
+#include "api.h"
+
+#endif // H_SUCC_HPP
---
+++ src/hardware/succ.cpp
@@ -0,0 +1,9 @@
+//* Conveyor and intake source file
+//* Created: 1/27/2021
+//* Last modified: 1/27/2021
+
+//* Main header
+#include "main.h"
+
+//* Hardware header
+#include "lib/hardware/succ.hpp"
