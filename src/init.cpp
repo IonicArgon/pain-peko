c9987eee6164bbadfae2248a2b6643ef4891007b
Marco Tan <marco.tan.200405@gmail.com>
2021-01-27 19:56:31 -0500

    init file

---
+++ src/init.cpp
@@ -0,0 +1,27 @@
+//* Initialization code
+//* Created: 1/27/2021
+//* Last modified: 1/27/2021
+
+//* Main header
+#include "main.h"
+
+//* Subsystem headers
+
+
+//* Initialization callback
+void initialize(void)
+{
+
+}
+
+//* Competition Initialize callback
+void competition_initialize(void)
+{
+
+}
+
+//* Disbled callback
+void disabled(void)
+{
+
+}
