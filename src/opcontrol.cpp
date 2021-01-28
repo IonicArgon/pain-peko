b36908ac6da6faf4f61561e4ac9a48e353cd933c
Marco Tan <marco.tan.200405@gmail.com>
2021-01-27 19:54:57 -0500

    teleop file

---
+++ src/opcontrol.cpp
@@ -0,0 +1,16 @@
+//* Teleop control code
+//* Created: 1/27/2021
+//* Last modified: 1/27/2021
+
+//* Main header
+#include "main.h"
+
+//* Subsystem headers
+
+
+
+//* Teleop callback
+void opcontrol(void)
+{
+
+}
