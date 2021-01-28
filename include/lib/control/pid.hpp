534e3377ad3ba9c3b73655c51241e06197b6a464
Marco Tan <marco.tan.200405@gmail.com>
2021-01-27 21:45:09 -0500

    pid base file

---
+++ include/lib/control/pid.hpp
@@ -0,0 +1,10 @@
+//* PID header file
+//* Created: 1/27/2021
+//* Last modified: 1/27/2021
+
+#ifndef H_PID_HPP
+#define H_PID_HPP
+
+#include "api.h"
+
+#endif // H_PID_HP
---
+++ src/control/pid.cpp
@@ -0,0 +1,10 @@
+//* PID source file
+//* Created: 1/27/2021
+//* Last modified: 1/27/2021
+
+//* Main header
+#include "main.h"
+
+//* Hardware header
+#include "lib/control/pid.hpp"
+#include "lib/hardware/drive.hpp"
