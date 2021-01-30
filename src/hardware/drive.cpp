2c90a9b3dd414d1adc10432ea17412f3fb28c5dc
Marco Tan <marco.tan.200405@gmail.com>
2021-01-30 18:54:36 -0500

    fix drive class constructor

--- src/hardware/drive.cpp
+++ src/hardware/drive.cpp
@@ -11,9 +11,9 @@
 Chassis::Chassis(
     int prm_mtr_lf, int prm_mtr_lb, int prm_mtr_rf, int prm_mtr_rb,
     int prm_trk_1a, int prm_trk_1b, int prm_trk_2a, int prm_trk_2b,
-    pros::motor_brake_mode_e    prm_brake = pros::motor_brake_mode_e::E_MOTOR_BRAKE_COAST,
-    pros::motor_encoder_units_e prm_unit = pros::motor_encoder_units_e::E_MOTOR_ENCODER_COUNTS,
-    pros::motor_gearset_e       prm_cart = pros::motor_gearset_e::E_MOTOR_GEARSET_18
+    pros::motor_brake_mode_e    prm_brake,
+    pros::motor_encoder_units_e prm_unit,
+    pros::motor_gearset_e       prm_cart
 )   : m_mtr_lf {prm_mtr_lf}, m_mtr_lb {prm_mtr_lb}, m_mtr_rf {prm_mtr_rf}, m_mtr_rb {prm_mtr_rb},
       m_trk_1a {prm_trk_1a}, m_trk_1b {prm_trk_1b}, m_trk_2a {prm_trk_2a}, m_trk_2b {prm_trk_2b},
       m_brake  {prm_brake},  m_unit   {prm_unit},   m_cart   {prm_cart}
