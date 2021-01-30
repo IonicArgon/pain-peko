9bc7e9a3deb2d4b6a48b66e2d77449cc6128455e
Marco Tan <marco.tan.200405@gmail.com>
2021-01-30 18:53:58 -0500

    conveyor and intake base class

--- include/lib/hardware/succ.hpp
+++ include/lib/hardware/succ.hpp
@@ -7,4 +7,27 @@
 
 #include "api.h"
 
+class Shooter
+{
+public:
+    Shooter(
+        int prm_mtr_ct, int prm_mtr_cb, int prm_mtr_il, int prm_mtr_ir,
+        pros::motor_brake_mode_e    prm_brake = pros::motor_brake_mode_e::E_MOTOR_BRAKE_BRAKE,
+        pros::motor_encoder_units_e prm_unit = pros::motor_encoder_units_e::E_MOTOR_ENCODER_COUNTS,
+        pros::motor_gearset_e       prm_cart = pros::motor_gearset_e::E_MOTOR_GEARSET_06
+    );
+    Shooter& set_brake(pros::motor_brake_mode_e prm_brake);
+    Shooter& set_unit(pros::motor_encoder_units_e prm_unit);
+    Shooter& set_cart(pros::motor_gearset_e prm_cart);
+    void shooter_set(int prm_set_ct, int prm_set_cb, int prm_set_ik = 0);
+
+private:
+    int m_mtr_ct, m_mtr_cb, m_mtr_il, m_mtr_ir;
+    pros::motor_brake_mode_e        m_brake;
+    pros::motor_encoder_units_e     m_unit;
+    pros::motor_gearset_e           m_cart;
+
+};
+
+
 #endif // H_SUCC_HPP
--- src/hardware/succ.cpp
+++ src/hardware/succ.cpp
@@ -7,3 +7,43 @@
 
 //* Hardware header
 #include "lib/hardware/succ.hpp"
+
+Shooter::Shooter(
+    int prm_mtr_ct, int prm_mtr_cb, int prm_mtr_il, int prm_mtr_ir,
+        pros::motor_brake_mode_e    prm_brake,
+        pros::motor_encoder_units_e prm_unit,
+        pros::motor_gearset_e       prm_cart
+)   : m_mtr_ct {prm_mtr_ct}, m_mtr_cb {prm_mtr_cb}, m_mtr_il {prm_mtr_il}, m_mtr_ir {prm_mtr_ir},
+      m_brake  {prm_brake},  m_unit   {prm_unit},   m_cart   {prm_cart}
+{
+    set_brake(prm_brake);
+    set_unit(prm_unit);
+    set_cart(prm_cart);
+}
+
+Shooter& Shooter::set_brake(pros::motor_brake_mode_e prm_brake)
+{
+    pros::c::motor_set_brake_mode(m_mtr_ct, prm_brake);
+    pros::c::motor_set_brake_mode(m_mtr_cb, prm_brake);
+    pros::c::motor_set_brake_mode(m_mtr_il, prm_brake);
+    pros::c::motor_set_brake_mode(m_mtr_ir, prm_brake);
+    return *this;
+}
+
+Shooter& Shooter::set_unit(pros::motor_encoder_units_e prm_unit)
+{
+    pros::c::motor_set_encoder_units(m_mtr_ct, prm_unit);
+    pros::c::motor_set_encoder_units(m_mtr_cb, prm_unit);
+    pros::c::motor_set_encoder_units(m_mtr_il, prm_unit);
+    pros::c::motor_set_encoder_units(m_mtr_ir, prm_unit);
+    return *this;
+}
+
+Shooter& Shooter::set_cart(pros::motor_gearset_e prm_cart)
+{
+    pros::c::motor_set_gearing(m_mtr_ct, prm_cart);
+    pros::c::motor_set_gearing(m_mtr_cb, prm_cart);
+    pros::c::motor_set_gearing(m_mtr_il, prm_cart);
+    pros::c::motor_set_gearing(m_mtr_ir, prm_cart);
+    return *this;
+}
