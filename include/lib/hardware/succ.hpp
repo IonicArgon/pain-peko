8c01a019e33509cddf1f3b71e83e4de98d255951
Marco Tan <marco.tan.200405@gmail.com>
2021-01-30 20:28:51 -0500

    date change

--- include/lib/hardware/succ.hpp
+++ include/lib/hardware/succ.hpp
@@ -1,6 +1,6 @@
 //* Conveyor and intake header file
 //* Created: 1/27/2021
-//* Last modified: 1/27/2021
+//* Last modified: 1/30/2021
 
 #ifndef H_SUCC_HPP
 #define H_SUCC_HPP
--- src/hardware/succ.cpp
+++ src/hardware/succ.cpp
@@ -1,6 +1,6 @@
 //* Conveyor and intake source file
 //* Created: 1/27/2021
-//* Last modified: 1/27/2021
+//* Last modified: 1/30/2021
 
 //* Main header
 #include "main.h"
