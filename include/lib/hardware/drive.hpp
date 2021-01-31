147a6bf2960e8f6f9277153f8b9637cbc6876f6c
Marco Tan <marco.tan.200405@gmail.com>
2021-01-30 20:28:23 -0500

    more missing things

--- include/lib/hardware/drive.hpp
+++ include/lib/hardware/drive.hpp
@@ -12,7 +12,7 @@ class Chassis
 public:
     Chassis(
         int prm_mtr_lf, int prm_mtr_lb, int prm_mtr_rf, int prm_mtr_rb,
-        int prm_trk_1a, int prm_trk_2a,
+        int prm_trk_l, int prm_trk_r,
         pros::motor_brake_mode_e    prm_brake = pros::motor_brake_mode_e::E_MOTOR_BRAKE_COAST,
         pros::motor_encoder_units_e prm_unit = pros::motor_encoder_units_e::E_MOTOR_ENCODER_COUNTS,
         pros::motor_gearset_e       prm_cart = pros::motor_gearset_e::E_MOTOR_GEARSET_18
@@ -21,10 +21,12 @@ public:
     Chassis& set_unit(pros::motor_encoder_units_e prm_unit);
     Chassis& set_cart(pros::motor_gearset_e prm_cart);
     Chassis& set_revr(int prm_mtr_lf, int prm_mtr_lb, int prm_mtr_rf, int prm_mtr_rb);
-    Chassis& conf_trk(int prm_trk_1a, int prm_trk_2a);
+    Chassis& conf_trk(int prm_trk_l, int prm_trk_r);
     void drive_mov(int prm_mov_l, int prm_mov_r);
     void drive_vel(int prm_vel_l, int prm_vel_r);
     void drive_vol(int prm_vol_l, int prm_vol_r);
+    void reset_trk(void);
+    int get_trk(char prm_trk_side);
 
 private:
     int m_mtr_lf, m_mtr_lb, m_mtr_rf, m_mtr_rb;
--- src/hardware/drive.cpp
+++ src/hardware/drive.cpp
@@ -10,18 +10,18 @@
 
 Chassis::Chassis(
     int prm_mtr_lf, int prm_mtr_lb, int prm_mtr_rf, int prm_mtr_rb,
-    int prm_trk_1a, int prm_trk_2a,
+    int prm_trk_l, int prm_trk_r,
     pros::motor_brake_mode_e    prm_brake,
     pros::motor_encoder_units_e prm_unit,
     pros::motor_gearset_e       prm_cart
 )   : m_mtr_lf {std::abs(prm_mtr_lf)}, m_mtr_lb {std::abs(prm_mtr_lb)}, m_mtr_rf {std::abs(prm_mtr_rf)}, m_mtr_rb {std::abs(prm_mtr_rb)},
-      m_brake  {prm_brake},  m_unit   {prm_unit},   m_cart   {prm_cart}
+      m_brake {prm_brake}, m_unit {prm_unit}, m_cart {prm_cart}
 {
     set_brake(prm_brake);
     set_unit(prm_unit);
     set_cart(prm_cart);
     set_revr(prm_mtr_lf, prm_mtr_lb, prm_mtr_rf, prm_mtr_rb);
-    conf_trk(prm_trk_1a, prm_trk_2a);
+    conf_trk(prm_trk_l, prm_trk_r);
 }
 
 Chassis& Chassis::set_brake(pros::motor_brake_mode_e prm_brake)
@@ -60,22 +60,24 @@ Chassis& Chassis::set_revr(int prm_mtr_lf, int prm_mtr_lb, int prm_mtr_rf, int p
     return *this;
 }
 
-Chassis& Chassis::conf_trk(int prm_trk_1a, int prm_trk_2a)
+Chassis& Chassis::conf_trk(int prm_trk_l, int prm_trk_r)
 {
-    pros::c::adi_port_set_config(std::abs(prm_trk_1a), pros::E_ADI_DIGITAL_IN);
-    pros::c::adi_port_set_config(std::abs(prm_trk_1a) + 1, pros::E_ADI_DIGITAL_IN);
-    pros::c::adi_port_set_config(std::abs(prm_trk_2a), pros::E_ADI_DIGITAL_IN);
-    pros::c::adi_port_set_config(std::abs(prm_trk_2a) + 1, pros::E_ADI_DIGITAL_IN);
+    pros::c::adi_port_set_config(std::abs(prm_trk_l), pros::E_ADI_DIGITAL_IN);
+    pros::c::adi_port_set_config(std::abs(prm_trk_l) + 1, pros::E_ADI_DIGITAL_IN);
+    pros::c::adi_port_set_config(std::abs(prm_trk_r), pros::E_ADI_DIGITAL_IN);
+    pros::c::adi_port_set_config(std::abs(prm_trk_r) + 1, pros::E_ADI_DIGITAL_IN);
+
     m_trk_l = pros::c::adi_encoder_init(
-        std::abs(prm_trk_1a),
-        std::abs(prm_trk_1a) + 1,
-        (prm_trk_1a > 0) ? false : true
+        std::abs(prm_trk_l),
+        std::abs(prm_trk_l) + 1,
+        (prm_trk_l > 0) ? false : true
     );
     m_trk_r = pros::c::adi_encoder_init(
-        std::abs(prm_trk_2a),
-        std::abs(prm_trk_2a) + 1,
-        (prm_trk_2a > 0) ? false : true
+        std::abs(prm_trk_r),
+        std::abs(prm_trk_r) + 1,
+        (prm_trk_r > 0) ? false : true
     );
+    reset_trk();
 }
 
 void Chassis::drive_mov(int prm_mov_l, int prm_mov_r)
@@ -100,4 +102,21 @@ void Chassis::drive_vol(int prm_vol_l, int prm_vol_r)
     pros::c::motor_move_voltage(m_mtr_lb, prm_vol_l);
     pros::c::motor_move_voltage(m_mtr_rf, prm_vol_r);
     pros::c::motor_move_voltage(m_mtr_rb, prm_vol_r);
+}
+
+void Chassis::reset_trk(void)
+{
+    pros::c::adi_encoder_reset(m_trk_l);
+    pros::c::adi_encoder_reset(m_trk_r);
+}
+
+int Chassis::get_trk(char prm_trk_side)
+{
+    switch (prm_trk_side)
+    {
+    case 'l':
+        return pros::c::adi_encoder_get(m_trk_l);
+    case 'r':
+        return pros::c::adi_encoder_get(m_trk_r);
+    }
 }
